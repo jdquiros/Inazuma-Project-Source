c16f36d74a168538c0f41c83a7784aa9b9e2c788
gjhbfdty2 <32973523+gjhbfdty2@users.noreply.github.com>
2017-11-01 15:07:03 -0700

    add placeholder idle/running animations

---
+++ Inazuma/Assets/scripts/animatorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animatorController : MonoBehaviour {
+
+	PlayerController playerController;
+	Animator anim;
+	Vector3 theScale;
+
+	private void Awake() {
+		playerController = GetComponentInParent<PlayerController> ();
+		anim = GetComponent<Animator> ();
+	}
+
+	void Start () {
+		theScale = transform.localScale;
+	}
+	
+	void FixedUpdate () {
+		float speed = playerController.getVelocity ().x;
+		int facing = playerController.getFacingDirection ();
+
+		anim.SetFloat ("Speed", Mathf.Abs (speed));
+	
+		//int facing -> 4 == left, 0 == right
+		if (facing == 0) {
+			transform.localScale = new Vector3 (theScale.x, transform.localScale.y, transform.localScale.z);
+		} else {
+			transform.localScale = new Vector3(-theScale.x, transform.localScale.y, transform.localScale.z);
+		}
+	}
+}
