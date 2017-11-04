c03910d4636ad35b24e7ead0ebf144e887c84ab1
Kevin Touch <ktouch@uci.edu>
2017-11-03 21:59:33 -0700

    Patrolling Enemy Movement

---
+++ Inazuma/Assets/Patrolling.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrolling : MonoBehaviour
+{
+
+    public float speed;
+    public float waitDuration;
+    private bool isWaiting;
+    private float waitTimer;
+
+    public Transform WayPointA;
+    public Transform WayPointB;
+
+    private Vector3 destination;
+    private Vector3 enemyPos;
+    private PlayerController playerController;
+    private Enemy enemy;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        destination = WayPointA.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isWaiting)
+        {
+            MoveToPoint(destination);
+        }
+          
+        if (waitTimer <= 0) {
+            isWaiting = false;
+        } else {
+            waitTimer -= Time.deltaTime;
+        }
+        
+
+    }
+
+    void MoveToPoint(Vector3 target)
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        Vector3 moveVector = direction * speed * Time.deltaTime;
+
+
+        if (Vector3.Distance(transform.position, target) < speed * Time.deltaTime)
+        {
+            transform.position = target;
+            waitTimer = waitDuration;
+            isWaiting = true;
+            if (destination == WayPointA.position) {
+                destination = WayPointB.position;
+            } else
+            {
+                destination = WayPointA.position;
+            }
+        }
+        else
+        {
+            transform.position += moveVector;
+        }
+    }
+
+}
