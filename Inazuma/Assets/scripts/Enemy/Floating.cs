c3900109d1b4f7051d20ad637efd97e424b4adcd
Kevin Touch <ktouch@uci.edu>
2017-11-10 18:01:55 -0800

    Enemies can be hit and knockback/damage player
    RIght now damage indicator is 0. Still need fixing on patrolling enemies.

--- Inazuma/Assets/scripts/Enemy/Floating.cs
+++ Inazuma/Assets/scripts/Enemy/Floating.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Floating : MonoBehaviour {
 
+    public Vector3 enemyPos;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = enemyPos + new Vector3(0, Mathf.Sin(3 * Time.time) / 2, 0);
+    }
+
+
 }
