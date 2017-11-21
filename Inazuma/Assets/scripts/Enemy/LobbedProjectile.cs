382cc1da8e6ddc7f805c5a4e5aff19cc316c6d95
Kyle Wong <kylewonga@gmail.com>
2017-11-20 22:32:25 -0800

    Added artillery turret

---
+++ Inazuma/Assets/scripts/Enemy/LobbedProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbedProjectile : Bullet {
+
+    // Use this for initialization
+    public float gravity;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.GetComponent<Rigidbody2D>().velocity += Vector2.down * gravity*Time.deltaTime;
+    }
+    public void setVelocity(Vector2 vel)
+    {
+        transform.GetComponent<Rigidbody2D>().velocity = vel;
+    }
+    
+    
+
+    
+}
