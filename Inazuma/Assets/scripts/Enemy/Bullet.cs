b0f7f4e7b6cb1e7ead6fa0ee9d4d812217c1c1d3
Kyle Wong <kylewonga@gmail.com>
2017-11-20 17:31:05 -0800

    Added bullets and a simple energy explosion effect

--- Inazuma/Assets/scripts/Enemy/Bullet.cs
+++ Inazuma/Assets/scripts/Enemy/Bullet.cs
@@ -6,15 +6,43 @@ public class Bullet : MonoBehaviour {
 
     // Use this for initialization
     private Vector3 moveDirection;
-
+    public float speed;
+    public int damage = 1;
 	void Start () {
         if (moveDirection == Vector3.zero)
             moveDirection = Vector3.right;
-        transform.GetComponent<Rigidbody2D>().velocity = moveDirection;
+        transform.GetComponent<Rigidbody2D>().velocity = moveDirection*speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    public void setVelocity(Vector3 newDir,float newSpeed)
+    {
+        moveDirection = newDir.normalized;
+        speed = newSpeed;
+        transform.GetComponent<Rigidbody2D>().velocity = moveDirection * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerController>().attemptDamagePlayer(damage);
+            collision.gameObject.GetComponent<PlayerController>().knockBackPlayer(transform.position);
+            kill();          
+        } else if (collision.gameObject.layer == LayerMask.NameToLayer("groundLayer"))
+        {
+            kill();
+        }
+
+    }
+   
+    public void kill()
+    {
+        GameObject explosion = (GameObject)Instantiate(Resources.Load("RadialExplosionRed"));
+        explosion.transform.position = transform.position;
+        Destroy(gameObject);
+    }
 }
