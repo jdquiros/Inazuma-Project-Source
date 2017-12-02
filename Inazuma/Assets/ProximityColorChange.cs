e211eb30ffb48c6ce5d2945494b07f4c4e852819
Kyle Wong <kylewonga@gmail.com>
2017-12-01 20:14:11 -0800

    Added floating text

---
+++ Inazuma/Assets/ProximityColorChange.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityColorChange : MonoBehaviour {
+
+    // Use this for initialization
+    public float activationDistance;
+    public Color startColor = Color.white;
+    public Color endColor = Color.white;
+    public float duration = 0;
+    private bool playerInRange = false;
+    private float colorChangeTimer = 0;
+    private TextMesh textMesh;
+    private Transform playerTransform;
+	void Start () {
+        if (duration <= 0)
+            duration = 0.001f;      //duration can't be zero, because div by zero errors
+
+        textMesh = GetComponent<TextMesh>();
+        textMesh.color = startColor;
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(transform.position,playerTransform.position) < activationDistance)
+        {
+            if(colorChangeTimer < duration)
+            {
+                colorChangeTimer += Time.deltaTime;
+            }
+        } else
+        {
+
+            if(colorChangeTimer > 0)
+            {
+                colorChangeTimer -= Time.deltaTime;
+            }
+        }
+        textMesh.color = Color.Lerp(startColor, endColor, colorChangeTimer / duration);
+
+    }
+}
---
+++ Inazuma/Assets/scripts/UIscripts/TextMeshColorLerp.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextMeshColorLerp : MonoBehaviour
+{
+
+    // Use this for initialization
+    public enum PlayTrigger
+    {
+        Start,
+        Enable,
+        None
+    }
+    public PlayTrigger playOn = PlayTrigger.Start;
+    public Color startColor = Color.white;
+    public Color endColor = Color.white;
+    public float duration = 0;
+    public float initialDelay = 0;
+    private TextMesh meshRenderer;
+    private IEnumerator colorCoroutine;
+    void Awake()
+    {
+
+        
+        meshRenderer = GetComponent<TextMesh>();
+        colorCoroutine = colorChange();
+
+    }
+    void OnEnable()
+    {
+        if (playOn == PlayTrigger.Enable)
+            startColorChange();
+    }
+    void Start()
+    {
+        if (playOn == PlayTrigger.Start)
+            startColorChange();
+    }
+
+
+    private IEnumerator colorChange()
+    {
+        meshRenderer.color = startColor;
+        while (initialDelay > 0)
+        {
+            initialDelay -= Time.deltaTime;
+            yield return null;
+        }
+        float elapsedTime = 0;
+        if (duration == 0)
+        {
+            meshRenderer.color = endColor;
+        }
+        while (elapsedTime < duration)
+        {
+            meshRenderer.color = Color.Lerp(startColor,
+                endColor, (elapsedTime / duration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        meshRenderer.color = endColor;
+    }
+    public void startColorChange()
+    {
+        StopCoroutine(colorCoroutine);
+        colorCoroutine = colorChange();
+        StartCoroutine(colorCoroutine);
+    }
+    public void setColors(Color newStartColor, Color newEndColor)
+    {
+        startColor = newStartColor;
+        endColor = newEndColor;
+    }
+}
+
+
