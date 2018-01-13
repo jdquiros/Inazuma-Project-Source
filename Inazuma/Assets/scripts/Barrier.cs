96e9d5a88bf5fcaf2be9faa023f23477bc8584b5
Kyle Wong <kylewonga@gmail.com>
2018-01-12 19:38:38 -0800

    Added Barriers

---
+++ Inazuma/Assets/scripts/Barrier.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrier : MonoBehaviour {
+
+    // Use this for initialization
+    private const int ACTIVE = 1;
+    private const int INACTIVE = 0;
+    private BoxCollider2D boxCollider;
+    public LightningLine[] lineList;
+    private LevelState stateData;
+    private bool active;
+	void Awake () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        stateData = GetComponent<LevelState>();
+        stateData.initialLoad();        //forces stateData to load its data, regardless of if its Awake() will be run in the future
+        active = (stateData.getLoadState() == 1);
+        if (active)
+        {
+            turnOnBarrier();
+        } else
+        {
+            turnOffBarrier();
+        }
+    }
+	
+    public void turnOffBarrier()
+    {
+        boxCollider.enabled = false;
+        for(int i = 0; i < lineList.Length; i++)
+        {
+            lineList[i].setIsDrawing(false);
+        }
+        stateData.setLoadState(INACTIVE);
+    }
+    public void turnOnBarrier()
+    {
+        boxCollider.enabled = true;
+        for (int i = 0; i < lineList.Length; i++)
+        {
+            lineList[i].setIsDrawing(true);
+        }
+        stateData.setLoadState(ACTIVE);
+
+    }
+}
--- Inazuma/Assets/scripts/LightningLine.cs
+++ Inazuma/Assets/scripts/LightningLine.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class LightningLine : MonoBehaviour {
 
     // Use this for initialization
+    public bool playOnAwake = true;
+
     public Transform endPointA;
     public Transform endPointB;
+    public bool capEndpoints = true;
     public int vertexCount = 1;
-    public int lineCount = 1;
     public float lineWidth;
     public float randomOffset;
     public float timeBetweenDraws;
@@ -19,40 +21,61 @@ public class LightningLine : MonoBehaviour {
     private Vector3 orthoVector;
     private Vector3 vectorFromA;
     private float stepSize;         //in percent
+    private bool isDrawing;
 
-	void Start () {
+	void Awake () {
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.positionCount = vertexCount;
         stepSize = 1f / vertexCount;
+        lineRenderer.positionCount = vertexCount;
+        isDrawing = playOnAwake;
 
     }
 
     // Update is called once per frame
     void LateUpdate () {
-        if (drawTimer > 0)
+        if (isDrawing)
         {
-            drawTimer -= Time.deltaTime;
-        }
-        else
-        {
-            for (int j = 0; j < lineCount; j++)
+            if (drawTimer > 0)
             {
-                lineRenderer.SetPosition(0, endPointA.position);
-                for (int i = 1; i < vertexCount - 1; i++)
-                {
-
-                    int rngSign = Random.value > 0.5f ? 1 : -1;
-                    point = Vector3.Lerp(endPointA.position, endPointB.position, i * stepSize + (randomOffset * Random.value * rngSign));
-                    vectorFromA = point - endPointA.position;
-                    orthoVector = new Vector3(-vectorFromA.y, vectorFromA.x, 0).normalized;
-                    rngSign = Random.value > 0.5f ? 1 : -1;
-                    point += orthoVector * (lineWidth / 2 * Random.value) * rngSign;
-                    lineRenderer.SetPosition(i, point);
-
-                }
-                lineRenderer.SetPosition(vertexCount - 1, endPointB.position);
-                drawTimer = timeBetweenDraws;
+                drawTimer -= Time.deltaTime;
             }
+            else
+            {
+                if (capEndpoints)
+                {
+                    lineRenderer.SetPosition(0, endPointA.position);
+                    drawLine(1, vertexCount - 1, stepSize);
+                    lineRenderer.SetPosition(vertexCount - 1, endPointB.position);
+                }
+                else
+                {
+                    drawLine(0, vertexCount, stepSize);
+                }
+                drawTimer = timeBetweenDraws;
+
+            }
+        } else
+        {
+            lineRenderer.positionCount = 0;
         }
+    }
+    private void drawLine(int minIndex, int maxIndex, float step)
+    {
+        for (int i = minIndex; i < maxIndex; i++)
+        {
+
+            int rngSign = Random.value > 0.5f ? 1 : -1;
+            point = Vector3.Lerp(endPointA.position, endPointB.position, i * step + (randomOffset * Random.value * rngSign));
+            vectorFromA = point - endPointA.position;
+            orthoVector = new Vector3(-vectorFromA.y, vectorFromA.x, 0).normalized;
+            rngSign = Random.value > 0.5f ? 1 : -1;
+            point += orthoVector * (lineWidth / 2 * Random.value) * rngSign;
+            lineRenderer.SetPosition(i, point);
+
+        }
+    }
+    public void setIsDrawing(bool x)
+    {
+        isDrawing = x;
     }
 }
