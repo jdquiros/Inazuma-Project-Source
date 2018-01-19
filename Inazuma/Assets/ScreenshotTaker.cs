f3bb3e1cb22f31a3e8cb51f98c4145b10c7739df
Kyle Wong <kylewonga@gmail.com>
2018-01-18 16:02:16 -0800

    Added screenshot taking script

---
+++ Inazuma/Assets/ScreenshotTaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotTaker : MonoBehaviour {
+
+    // Use this for initialization
+    private int screenshotCount = 0;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            ScreenCapture.CaptureScreenshot("Screenshot"+screenshotCount,4);
+            screenshotCount++;
+        }
+	}
+}
