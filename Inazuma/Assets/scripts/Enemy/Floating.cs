8500a9b411f213ae80c270f05070b3ac1a1b8c16
Kyle Wong <kylewonga@gmail.com>
2018-01-19 21:16:09 -0800

    Added inspector variables to float script

--- Inazuma/Assets/scripts/Enemy/Floating.cs
+++ Inazuma/Assets/scripts/Enemy/Floating.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Floating : MonoBehaviour {
 
     private Vector3 enemyPos;
-
+    public float heightRange = .3f;
+    public float periodSpeed = 3f;
+    public bool randomOffset;
+    private float periodOffset;
 	// Use this for initialization
 	void Start () {
+        
         enemyPos = transform.position;
+        if (randomOffset)
+            periodOffset = Random.value * Mathf.PI * 2;
+
 	}
 
     // Update is called once per frame
     void Update()
     {
-        transform.localPosition = enemyPos + new Vector3(0, Mathf.Sin(3 * Time.time) / 2, 0);
+        transform.localPosition = enemyPos + new Vector3(0, Mathf.Sin(periodSpeed * Time.time + periodOffset) * heightRange, 0);
     }
 
 
