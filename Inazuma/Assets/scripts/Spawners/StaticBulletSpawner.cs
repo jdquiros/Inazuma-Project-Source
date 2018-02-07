24bf2fd63aa4f5fd85c295684088346f4bf79278
gjhbfdty2 <32973523+gjhbfdty2@users.noreply.github.com>
2018-02-06 17:48:50 -0800

    make static turrets shoot in the correct direction

--- Inazuma/Assets/scripts/Spawners/StaticBulletSpawner.cs
+++ Inazuma/Assets/scripts/Spawners/StaticBulletSpawner.cs
@@ -13,8 +13,21 @@ public class StaticBulletSpawner : MonoBehaviour {
     public Transform bulletType;        //drag prefab here
     public float xOffset;
 
+	private Vector3 angles;
+
 	void Start () {
         InvokeRepeating("fireBullet", 0, 60 / fireRate);
+
+		angles = transform.eulerAngles;    //finds the angle to shoot based on the Turret's rotation
+		if (angles.z == 0.0f) {
+			angles = Vector3.right;
+		} else if (angles.z == 90.0f) {
+			angles = Vector3.up;
+		} else if (angles.z == 180.0f) {
+			angles = Vector3.left;
+		} else {
+			angles = Vector3.down;
+		}
 	}
 	
 	// Update is called once per frame
@@ -24,7 +37,8 @@ public class StaticBulletSpawner : MonoBehaviour {
 
     private void fireBullet()
     {
-        GameObject bullet = Instantiate(bulletType).gameObject;
-        bullet.transform.position = new Vector3(transform.position.x + xOffset, transform.position.y, 0);
-    }
+		GameObject bullet = Instantiate(bulletType, transform.position, transform.rotation).gameObject;
+
+		bullet.GetComponent<Bullet> ().setVelocity (angles, 10.0f); //sets shooting angle. 10 is default speed
+	}
 }
