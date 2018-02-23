44377f7218c1f5f128ea3eb21853d8f8bedf9611
Kyle Wong <kylewonga@gmail.com>
2018-02-22 16:49:07 -0800

    Small bug fixes

--- Inazuma/Assets/scripts/Spawners/FloatingEnemySpawner.cs
+++ Inazuma/Assets/scripts/Spawners/FloatingEnemySpawner.cs
@@ -6,10 +6,8 @@ public class FloatingEnemySpawner : EnemySpawner {
 
     public override void spawnEnemy()
     {
-        GameObject explosion = (GameObject)Instantiate(Resources.Load("RadialExplosionRed"));
-        explosion.transform.position = transform.position;
-        GameObject newEnemy = Instantiate(spawnPrefab).gameObject;
-        newEnemy.transform.position = transform.position;
+        GameObject explosion = (GameObject)Instantiate(Resources.Load("RadialExplosionRed"),transform.position,transform.rotation);
+        GameObject newEnemy = Instantiate(spawnPrefab,transform.position,transform.rotation).gameObject;
 
         newEnemy.GetComponent<Enemy>().spawnedBy = this;
 
